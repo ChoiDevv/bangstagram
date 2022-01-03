5b97ad068c31aa0d632964050ccf10eeeb5654f3
csd <yuri1337@naver.com>
2022-01-03 10:24:07 +0900

    app.py port correction

--- app.py
+++ app.py
@@ -265,4 +265,4 @@ if __name__ == '__main__':
     #print(dir(db_post.postinfo))
     #print(db_post.postinfo.count_documents({}))
     #print(db.userinfo.count_documents({}))
-    app.run('0.0.0.0', port=5001, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
